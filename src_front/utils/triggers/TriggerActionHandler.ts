c3b0585e9268bb3adc50550b5e913d9df8a83005
Durss <francois.dursus@gmail.com>
2026-04-03 01:38:02 +0200

    feat(triggers): adding possibility to toggle a folder of triggers from the triggers
    fix: broken triggerLogs reactivity
    fix: trigger logs "loading" state will now properly update
    fix: hide "condition" button on light trigger actions if there already are conditions
    fix: waiting for vueSelect component to be mounted on ParamItem
    refactor: cleaning up $emit and $t calls in templates
    chore: adding vue-select TS definitions on remote repo
    chore: bumping lodash dep

--- src_front/utils/triggers/TriggerActionHandler.ts
+++ src_front/utils/triggers/TriggerActionHandler.ts
@@ -2529,7 +2529,7 @@ export default class TriggerActionHandler {
 			}
 		}
 
-		const log: LogTrigger = {
+		let log: LogTrigger = {
 			date: Date.now(),
 			id: Utils.getUUID(),
 			trigger,
@@ -2580,7 +2580,8 @@ export default class TriggerActionHandler {
 			TriggerTypes.TWITCHAT_LIVE_FRIENDS,
 			TriggerTypes.TWITCHAT_MESSAGE,
 		];
-		if (!noLogs.includes(trigger.type)) Logger.instance.log("triggers", log);
+		if (!noLogs.includes(trigger.type))
+			log = Logger.instance.log("triggers", log) as LogTrigger;
 
 		//Special case for friends stream start/stop notifications
 		if (trigger.type == TriggerTypes.TWITCHAT_LIVE_FRIENDS) {
@@ -4683,6 +4684,39 @@ export default class TriggerActionHandler {
 										'". New State is: ' +
 										trigger.enabled,
 								});
+							}
+						} else {
+							const entry = TriggerUtils.getFlatFolderList().find(
+								(v) => v.id == step.triggerId,
+							);
+							logStep.messages.push({
+								date: Date.now(),
+								value: `Search for trigger folder ID "${step.triggerId}"`,
+							});
+							if (entry) {
+								switch (step.action) {
+									case "enable":
+										entry.enabled = true;
+										break;
+									case "disable":
+										entry.enabled = false;
+										break;
+									case "toggle":
+										entry.enabled = !entry.enabled;
+										break;
+								}
+								sTriggers.computeTriggerTreeEnabledStates();
+								logStep.messages.push({
+									date: Date.now(),
+									value: `${step.action} folder "${step.triggerId}". New State is: ${entry.enabled}`,
+								});
+							} else {
+								logStep.messages.push({
+									date: Date.now(),
+									value: `❌ No trigger and no folder found with ID "${step.triggerId}"`,
+								});
+								log.error = true;
+								logStep.error = true;
 							}
 						}
 					}
